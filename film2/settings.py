9b0d283de53b03ae5d896a058f5ab78fa2c039bf
chaizhikuan <399578426@qq.com>
2018-12-18 17:34:01 +0800

    done

--- film2/settings.py
+++ film2/settings.py
@@ -89,4 +89,7 @@ ITEM_PIPELINES = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
 IMAGES_STORE='D:'
+
+#test
